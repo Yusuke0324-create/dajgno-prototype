16ff25af6fd8be6bb21cbd68e6ac9ad6d84fe981
“Yusuke0324-create” <hamupupp0324@gmail.com>
2025-04-21 01:08:46 +0900

    '二回目です'

--- lunchmap/urls.py
+++ lunchmap/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+
 #top_page.htmlとリンクしている。top_pageでappp_folderとなっている部分をlunchmapに直せばここをapp_name = 'lunchmap'にできるが、まだやっていない
 app_name = 'app_folder'
 
 urlpatterns = [
     path('top_page/', views.top_page, name='top_page')
+
 ]
--- lunchmap/views.py
+++ lunchmap/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
+from django.views import generic  
+from django.views import View  
+from .models import Shop
 
-# Create your views here.
-from django.urls import reverse_lazy
-from django.views import generic
-from django .views import View
-from .models import Category, Shop
+class SampleView(View):  #一覧表示、検索機能のトップページ
+	def get(self, request, *args, **kwargs): 
+		shops = Shop.objects.all()
+		context = {
+            'object_list': shops,
+        }
+		return render(request, 'app_folder/page01.html', context=context)
 
+	def post(self, request, *args, **kwargs):  
+		input_data = request.POST['input_data']
+		result = Shop.objects.filter(name=input_data)
 
-class SampleView(View):  
-	def get(self, request, *args, **kwargs):  
-		return render(request, 'app_folder/top_page.html')
+		if result.exists():
+			result_name = result[0].name
+			result_address = result[0].address
+		else:
+			result_name = '該当する項目はありませんでした'
+			result_address = ''
+
+		context = {
+    		'result_name': result_name,
+    		'result_address': result_address,
+		}
+		return render(request, 'app_folder/page02.html', context=context)
+
+		
 top_page = SampleView.as_view()
 
-    
+class DetailView(generic.DetailView):
+	model = Shop

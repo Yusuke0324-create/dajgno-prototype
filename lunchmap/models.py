ae27886bf1eacac70e1bb231c6a2e92f2c703687
“Yusuke0324-create” <hamupupp0324@gmail.com>
2025-05-23 03:05:43 +0900

    'template変えた'

--- lunchmap/models.py
+++ lunchmap/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -34,8 +35,10 @@ class Shop(models.Model):
     
 
 class Blog(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)  # 関連店舗
     title = models.CharField(max_length=200)
     content = RichTextUploadingField()  # 画像挿入が可能なリッチテキストエリア
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # 誰が投稿したか
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
